c92cd5ef2e91ce1f28a64b6646bb825547fd4549
gaohuikai <gaohuikai@xforceplus.com>
2019-01-28 16:07:25 +0800

    pro update style

--- config.js
+++ config.js
@@ -35,7 +35,7 @@ const config = {
   titlename: 'XforceCloud', // html title
   favicon: 'favicon.ico', // page favicon
   theme: { // less/sass modify vars
-    'primary-color': '#00896C',
+    'primary-color': '#3f51b5',
     'icon-font-size-base': '16px',
   },
   dashboard: {
